3c5ceb64266aa5908eda1f79af7cd538ba004342
super <super@JDDBJ1PF180TCV.360buyAD.local>
2021-02-04 17:27:03 +0800

    add

--- deploy-u.bat
+++
@@ -1,4 +0,0 @@
-git remote add -f docs/.vuepress/dist https://github.com/gcblock/docs.git
-git subtree add --prefix=docs/.vuepress/dist dist master --squash
-git subtree pull --prefix=docs/.vuepress/dist https://github.com/gcblock/doc.git master --squash
-git subtree push --prefix=docs/.vuepress/dist https://github.com/gcblock/doc.git master
